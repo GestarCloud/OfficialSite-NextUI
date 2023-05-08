3ac4e9ee5a1b66e1ca697fc619c4e837114271f9
HydroGest <2445691453@qq.com>
2023-05-08 22:29:28 +0800

    Add status link

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,15 @@ import { Navbar, Button, Link, Text } from "@nextui-org/react";
 
 export default function AppBar() {
   return (
-     <Navbar isBordered variant='static'>
+     <Navbar isBordered variant='sticky'>
         <Navbar.Brand>
           <Text b color="inherit" hideIn="xs">
             GESTAR CLOUD
           </Text>
         </Navbar.Brand>
+        <Navbar.Content hideIn="xs">
+          <Navbar.Link href="https://status.gestar.drive">监控</Navbar.Link>
+         </Navbar.Content>
         <Navbar.Content>
           <Navbar.Item>
             <Button auto flat as={Link} onClick={() =>window.location.href='https://app.gestar.cloud/'}>
